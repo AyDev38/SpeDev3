c32dfa498c0e28902034250aabf23553c824ed72
AymeRx <aymeric.surre@gmail.com>
2024-12-17 15:32:57 +0100

    Blocage de la page projects pour des users non connecté

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { useAuthStore } from '@/stores/authStore';
+
 import HomePage from '../views/HomePage.vue';
 import LoginPage from '../views/LoginPage.vue';
 import RegisterPage from '../views/RegisterPage.vue';
@@ -6,16 +8,34 @@ import ProjectPage from '../views/ProjectPage.vue';
 import NotFoundPage from '../views/NotFoundPage.vue';
 
 const routes = [
-  { path: '/', component: HomePage },
-  { path: '/login', component: LoginPage },
-  { path: '/register', component: RegisterPage },
-  { path: '/projects', component: ProjectPage },
+  { path: '/', component: HomePage, meta: { requiresGuest: true } },
+  { path: '/login', component: LoginPage, meta: { requiresGuest: true } },
+  { path: '/register', component: RegisterPage, meta: { requiresGuest: true } },
+  { path: '/projects', component: ProjectPage, meta: { requiresAuth: true } },
   { path: '/:pathMatch(.*)*', component: NotFoundPage },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+// Guard global pour la navigation
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+
+  // Si la route nécessite que l'utilisateur soit non connecté
+  if (to.meta.requiresGuest && authStore.currentUser) {
+    next('/projects'); // Rediriger les utilisateurs connectés vers la page des projets
+  }
+  // Si la route nécessite que l'utilisateur soit connecté
+  else if (to.meta.requiresAuth && !authStore.currentUser) {
+    next('/login'); // Rediriger les utilisateurs non connectés vers la page de connexion
+  }
+  // Sinon, on continue la navigation
+  else {
+    next();
+  }
 });
 
 export default router;
